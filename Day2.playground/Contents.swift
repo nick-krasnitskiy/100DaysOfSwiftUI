3cd9a9e7a38f935e0decae3470b8481cb355b53d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-11 15:01:52 +0300

    Add store truth with Booleans.

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -1,3 +1,27 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let goodDogs = true
+let gameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOverTwo = false
+print(gameOverTwo)
+
+gameOverTwo.toggle()
+print(gameOverTwo)
+
