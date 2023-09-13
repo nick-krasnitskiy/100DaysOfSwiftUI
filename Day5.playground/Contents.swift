e321745b00b9cc03adde79a6fb90434f8618e48e
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-13 13:24:18 +0300

    Check a condition is true or false.

---
+++ Day5.playground/Contents.swift
@@ -0,0 +1,100 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let score = 85
+
+if score > 80 {
+    print("Great job!")
+}
+
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote")
+}
+
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimmer"
+
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+// Make an array of 3 numbers
+var numbers = [1, 2, 3]
+
+// Add a 4th
+numbers.append(4)
+
+// If we have over 3 items
+if numbers.count > 3 {
+    // Removw the oldest number
+    numbers.remove(at: 0)
+}
+
+// Display the result
+print(numbers)
+
+let country = "Canada"
+
+if country == "Australia" {
+    print("G'day!")
+}
+
+let name = "Taylor Swift"
+
+if name  != "Anonymous" {
+    print("Welcome, \(name)!")
+}
+
+// Create the username variable
+var username = "taylorswift13"
+
+// If username contains an empty string
+if username.isEmpty {
+    // Make it equal to Anonymous
+    username = "Anonymous"
+}
+
+// Now print a welcome message
+print("Welcome, \(username)!")
+
+let firstName = "Paul"
+let secondName = "Sophie"
+
+let firstAge = 40
+let secondAge = 10
+
+print(firstName == secondName)
+print(firstName != secondName)
+print(firstName < secondName)
+print(firstName >= secondName)
+
+print(firstAge == secondAge)
+print(firstAge != secondAge)
+print(firstAge < secondAge)
+print(firstAge >= secondAge)
+
+enum Sizes: Comparable {
+    case small, medium, large
+}
+
+let first = Sizes.small
+let second = Sizes.large
+
+print(first < second)
