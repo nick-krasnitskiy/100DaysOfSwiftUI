a5b79c7ae9ef50ef96ff47b2ac2bfd23e6418390
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-14 13:46:26 +0300

    Add check multiple conditions.

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -98,3 +98,70 @@ let first = Sizes.small
 let second = Sizes.large
 
 print(first < second)
+
+let ageTwo = 16
+
+if ageTwo >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let a = false
+let b = true
+
+if a {
+    print("Code is run if a is true")
+} else if b {
+    print("Code is run if a is false but b is true")
+} else {
+    print("Code is run if both a and b are false")
+}
+
+let temp = 25
+
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day")
+    }
+}
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path...")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+let scoreTwo = 9001
+
+if scoreTwo > 9000 {
+    print("It's over 9000!")
+} else if scoreTwo == 9000 {
+    print("It's exactly 9000!")
+} else {
+    print("It's not over 9000!")
+}
+
+
+
