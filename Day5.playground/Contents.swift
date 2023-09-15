833a7a13987dd84a757390fa410459d6260d313a
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-15 10:41:03 +0300

    Add use the ternary conditional operator.

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -228,3 +228,31 @@ case 2:
 default:
     print("A partridge in a pear tree")
 }
+
+let ageThree = 18
+let canVote = ageThree >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "black" : "white"
+print(background)
+
+let isAuthenticated = true
+print(isAuthenticated ? "Welcome" : "Who are you?")
+
+if isAuthenticated {
+    print("Welcome")
+} else {
+    print("Who are you?")
+}
