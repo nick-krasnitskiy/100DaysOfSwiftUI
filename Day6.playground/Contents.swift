c9d7d7ddfabcad04cfdf31cd2248ce88e4d5bc43
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-15 14:41:42 +0300

    Add the solve of checkpoint.

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -135,3 +135,18 @@ outerLoop: for option1 in options {
     }
 }
 
+for i in 1...100 {
+    
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+        continue
+    }else if i.isMultiple(of: 3) {
+        print("Fizz")
+        continue
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+        continue
+    }
+    
+    print(i)
+}
