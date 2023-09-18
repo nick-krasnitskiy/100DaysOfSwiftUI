8ed531a8c341f5a663d2dfc07540aae5cf63e5ac
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-18 15:20:06 +0300

    Customize parameter labels.

--- Day7.playground/Contents.swift
+++ Day7.playground/Contents.swift
@@ -105,3 +105,37 @@ print("Name: \(firstName2) \(lastName2)")
 
 let (firstName3, _) = getUser()
 print("Name: \(firstName3)")
+
+func rollDice(sides: Int, count: Int) -> [Int] {
+    // Start with an empty array
+    var rolls = [Int]()
+    
+    // Roll as many dice as needed
+    for _ in 1...count {
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+    
+    // Send back all the rolls
+    return rolls
+}
+ 
+let rolls = rollDice(sides: 6, count: 4)
+
+let lyric = "I see a red door and I want it painted black"
+print(lyric.hasPrefix("I see"))
+ 
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+print(isUppercase(string))
+
+func printTimesTablesTwo(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTablesTwo(for: 5)
