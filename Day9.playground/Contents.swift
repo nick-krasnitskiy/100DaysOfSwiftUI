e4ba846901903efa74c8bdb68d8bc8496856dac1
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-09-22 08:36:02 +0300

    Accept functions as parameters.

--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -109,3 +109,51 @@ func animate(duration: Double, animations: () -> Void) {
 
 animate(duration: 3) { print("Fade out of the image") }
 
+func greetUser2() {
+    print("Hi there!")
+}
+
+greetUser2()
+
+var greetUser2Copy: () -> Void = greetUser2
+greetUser2Copy()
+
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+
+let rolls = makeArray(size: 50) { Int.random(in: 1...20) }
+print(rolls)
+
+func generateNumber() -> Int {
+    Int.random(in: 1...20)
+}
+
+let rolls2 = makeArray(size: 50, using: generateNumber)
+print(rolls2)
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third: {
+    print("This is the third work")
+}
+
