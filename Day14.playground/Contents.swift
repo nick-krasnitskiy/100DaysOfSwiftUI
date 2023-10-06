7d972fcf9bcce6e3dd95eea067b60322f0623e3c
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-10-06 08:33:39 +0300

    Unwrap optionals with nil coalescing.

--- Day14.playground/Contents.swift
+++ Day14.playground/Contents.swift
@@ -50,4 +50,28 @@ func printSquare(of number: Int?) {
     print("\(number) X \(number) is \(number * number)")
 }
 
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+let new = captains["Serenity"] ?? "N/A"
+let new2 = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favourite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number2 = Int(input) ?? 0
+print(number2)
 
