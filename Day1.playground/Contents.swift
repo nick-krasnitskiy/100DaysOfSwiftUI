8f0cd56e65f3818a8d676b3f92de821b9bb24b14
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-01-27 13:47:26 +0300

    How to store whole numbers.

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -58,5 +58,38 @@ O’ mice and men
 Gang aft agley
 """
 
+// How to store whole numbers
+
+let score = 10
+let reallyBig = 100_000_000
+let reallyBigTwo = 1__000__000___000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+
+counter -= 10
+print(counter)
+
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
 
 
