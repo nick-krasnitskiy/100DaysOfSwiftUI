a39e3a2f77ee4e093b034cf87eebab51db6af66e
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-01-27 10:16:54 +0300

    Initial Commit

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,26 @@
+// How to create variables and constants
+
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+// character = "Elloise" - we can't change constants
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Skott"
+let dogBreed = "Samoed"
+let meaningOfLife = "How many roads must a man walk down?"
+
