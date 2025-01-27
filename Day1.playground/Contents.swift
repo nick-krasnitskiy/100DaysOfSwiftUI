68d65abe953e32d825a1e67450f2dcaed15ba12f
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-01-27 15:38:51 +0300

    How to store decimal numbers.

--- Day1.playground/Contents.swift
+++ Day1.playground/Contents.swift
@@ -92,4 +92,27 @@ print(number.isMultiple(of: 3))
 
 print(120.isMultiple(of: 3))
 
+// How to store decimal numbers
+
+let numberTwo = 0.1 + 0.2
+print(numberTwo)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+let d = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var nameTwo = "Nicolas Cage"
+nameTwo = "John Travolta"
+
+var rating = 5.0
+rating *= 2
+
+
+
 
