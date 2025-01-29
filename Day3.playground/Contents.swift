af4f01d9465f0d3741cccbaab2b068e77bdc5f46
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-01-29 14:43:37 +0300

    How to store and find data in dictionaries.

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -65,3 +65,60 @@ print(cities.sorted())
 let presidents = ["Bush", "Obama", "Trump", "Biden"]
 let reversedPresidents = presidents.reversed()
 print(reversedPresidents)
+
+// How to store and find data in dictionaries
+
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+               
+print("Name: \(employee[0])")
+employee.remove(at: 1)
+print("Job title: \(employee[1])")
+// print("Location: \(employee[2])")
+
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+print(employee2["name"])
+print(employee2["job"])
+print(employee2["location"])
+
+print(employee2["password"])
+print(employee2["status"])
+print(employee2["manager"])
+
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Umknown"])
+print(employee2["location", default: "Unknown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2020: "Tokyo"
+]
+
+print(olympics[2020, default: "Unkhown"])
+
+var heights = Dictionary<String, Int>()
+var heights2 = [String: Int]()
+
+heights2["Yao Ming"] = 229
+heights2["Shaquille O'Neal"] = 216
+heights2["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+
+archEnemies["Batman"] = "Penguin"
