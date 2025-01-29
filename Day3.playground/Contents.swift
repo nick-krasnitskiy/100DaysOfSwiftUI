bb86672db5cc739cd8e19718cb60ab08878d6a60
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-01-29 16:33:01 +0300

    How to use sets for fast data lookup.

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -122,3 +122,14 @@ archEnemies["Batman"] = "The Joker"
 archEnemies["Superman"] = "Lex Luthor"
 
 archEnemies["Batman"] = "Penguin"
+
+// How to use sets for fast data lookup
+
+let people = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+print(people)
+
+var people2 = Set<String>()
+people2.insert("Denzel Washington")
+people2.insert("Tom Cruise")
+people2.insert("Nicolas Cage")
+people2.insert("Samuel L Jackson")
