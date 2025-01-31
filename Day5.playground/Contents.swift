49c2f34a1f14ead763165e5c282a634bfa0d1396
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-01-31 08:31:17 +0300

    How to check a condition is true or false.

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -1,3 +1,97 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// How to check a condition is true or false
+
+let score = 85
+
+if score > 80 {
+    print("Great job!")
+}
+
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
+
+if percentage < 80 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote")
+}
+
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimmer"
+
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+
+// Make an array of 3 numbers
+var numbers = [1, 2, 3]
+
+// Add a 4th
+numbers.append(4)
+
+// If we have over 3 items
+if numbers.count > 3 {
+    // Remove the oldest number
+    numbers.remove(at: 0)
+}
+
+// Display the result
+print(numbers)
+
+
+let country = "Canada"
+
+if country == "Australia" {
+    print("G' day!")
+}
+
+let name = "Taylor Swift"
+
+if name != "Anonymous" {
+    print("Welcome, \(name)")
+}
+
+// Create the username variable
+var username = "taylorswift13"
+
+// If username contains an empty string
+if username == "" {
+    // Make it equal to "Anonymous"
+    username = "Anonymous"
+}
+
+// Now print a welcome message
+print("Welcome, \(username)!")
+
+// Better solutuon
+if username.count == 0 {
+    username = "Anonymous"
+}
+
+// The best solution
+if username.isEmpty {
+    username = "Anonymous"
+}
+
+enum Sizes: Comparable {
+    case small, medium, large
+}
+
+let first = Sizes.small
+let second = Sizes.medium
+print(first < second)
