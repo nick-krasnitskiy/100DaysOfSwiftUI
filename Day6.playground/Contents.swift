b0d21224f3ac17e71d94efe6e4f160493ae35b73
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-03 11:09:48 +0300

    How to skip loop items with break and continue.

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -58,3 +58,88 @@ print(names2[0])
 print(names2[1...3])
 print(names2[1...])
 
+// How to use a while loop to repeat work
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+let id = Int.random(in: 1...1000)
+let amount = Double.random(in: 0...1)
+
+// create an integer to store out roll
+var roll = 0
+
+// carry on looping untill we reach 20
+while roll != 20 {
+    // roll a new dice and print what it was
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+// if we're here it means thre loop ended - we got a 20!
+print("Critical hit!")
+
+// How to skip loop items with break and continue
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+    
+    print("Found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+        
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+let scores = [1, 8, 4, 3, 0, 5, 2]
+var count = 0
+
+for score in scores {
+    if score == 0 {
+        break
+    }
+    
+    count += 1
+}
+
+print("You had \(count) scores before you got 0.")
+
+let options = ["up", "down", "left", "right"]
+let secretCombination = ["up", "up", "right"]
+
+outerLoop: for option1 in options {
+    for option2 in options {
+        for option3 in options {
+            print("In loop")
+            
+            let attempt = [option1, option2, option3]
+            
+            if attempt == secretCombination {
+                print("The combination is \(attempt)")
+                break outerLoop
+            }
+        }
+    }
+}
