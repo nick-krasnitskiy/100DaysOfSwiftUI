920e345c8b45836fbd6b28b79537efd44469e004
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-05 10:03:12 +0300

    How to handle errors in functions.

--- Day8.playground/Contents.swift
+++ Day8.playground/Contents.swift
@@ -26,3 +26,51 @@ func findDirections(from: String, to: String, route: String = "fastest", avoidHi
 findDirections(from: "London", to: "Glasgow")
 findDirections(from: "London", to: "Glasgow", route: "scenic")
 findDirections(from: "London", to: "Glasgow", route: "scenic", avoidHighways: true)
+
+// How to handle errors in functions
+
+// Step 1. Telling Swift about the possible errors that can happen. It doesn’t define what those mean, only that they exist.
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+// Step 2. Write a function that will trigger one of those errors.When an error is triggered – or thrown in Swift – we’re saying something fatal went wrong with the function, and instead of continuing as normal it immediately terminates without sending back any value. In our case, we’re going to write a function that checks the strength of a password.
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+// Step 3. Run the function and handle any errors that might happen.
+
+// Starting a block of work that might throw errors, using do.
+// Calling one or more throwing functions, using try.
+// Handling any thrown errors using catch.
+
+let string = "12345"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Pleasure use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print(error.localizedDescription)
+}
+
