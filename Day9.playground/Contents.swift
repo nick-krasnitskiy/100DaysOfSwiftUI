c038e40975e68659f931a813cd1eac5d04821bb7
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-06 14:53:11 +0300

    How to use trailing closures and shorthand syntax.

--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -81,3 +81,34 @@ let payment3 = { () -> Bool in
 }
 
 payment3()
+
+// How to use trailing closures and shorthand syntax
+
+let captainFirstTeam3 = team.sorted { name1, name2 in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+}
+
+let captainFirstTeam4 = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+    
+    return $0 < $1
+}
+
+let reverseTeam = team.sorted { $0 > $1 }
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
+
