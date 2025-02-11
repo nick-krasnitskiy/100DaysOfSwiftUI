def834299e4a75dd3995a71ac453d520489b1e1d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-11 09:11:33 +0300

    How to limit access to internal data using access control.

--- Day11.playground/Contents.swift
+++ Day11.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// How to limit access to internal data using access control
+
+struct BankAccount {
+    private(set) var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+
+var account = BankAccount()
+account.deposit(amount: 100)
+
+let succes = account.withdraw(amount: 200)
+
+if succes {
+    print("Withdrew money sucessfully")
+} else {
+    print("Failed to get the money")
+}
+
+// account.funds -= 100
+
