32ce01dae13c81780ea02e8abdbb37c04f126bf6
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-14 09:48:39 +0300

    How to create and use protocols.

--- Day13.playground/Contents.swift
+++ Day13.playground/Contents.swift
@@ -1,3 +1,102 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// How to create and use protocols
+
+protocol Vehicle {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distanse: Int)
+}
+
+struct Car: Vehicle {
+    let name = "Car"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    
+    func travel(distanse: Int) {
+        print("I'm driving \(distanse) km.")
+    }
+    
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+}
+
+func commute(distance: Int, using vehicle: Vehicle) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distanse: distance)
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
+
+struct Bicycle: Vehicle {
+    let name = "Bicycle"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    
+    func travel(distanse: Int) {
+        print("I'm cycling \(distanse) km.")
+    }
+}
+
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+func getTravetEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance) km")
+    }
+}
+
+getTravetEstimates(using: [car, bike], distance: 150)
+
+struct Book {
+    var name: String
+}
+
+func buy(_ book: Book) {
+    print("I'm buying \(book.name)")
+}
+
+protocol Purchaseable {
+    var name: String { get set }
+}
+
+struct Book2: Purchaseable {
+    var name: String
+    var author: String
+}
+
+struct Movie: Purchaseable {
+    var name: String
+    var actors: [String]
+}
+
+struct Car2: Purchaseable {
+    var name: String
+    var manufacturer: String
+}
+
+struct Coffee: Purchaseable {
+    var name: String
+    var strenght: Int
+}
+
+func buy2(_ item: Purchaseable) {
+    print("I'm biying \(item.name)")
+}
+
