a6b77b8725efe2aecb0b6e588a47b901ea211cf6
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-15 21:11:09 +0300

    How to create and use protocol extensions.

--- Day13.playground/Contents.swift
+++ Day13.playground/Contents.swift
@@ -171,3 +171,39 @@ extension Book3 {
         self.readingHours = pageCount / 50
     }
 }
+
+// How to create and use protocol extensions
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if !guests.isEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
