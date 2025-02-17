4fe4547a9a9aa5b1a760bc2fab8bc0387d1bba09
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-17 08:14:30 +0300

    How to handle missing data with optionals.

--- Day14.playground/Contents.swift
+++ Day14.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// How to handle missing data with optionals
+
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+let peachOpposite = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+var number: Int? = nil
+
+if let number {
+    print(square(number: number))
+}
+
