b19de0f6532c3fa509211c8eb4eccf6f240676c8
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-21 08:10:57 +0300

    Initial commit.

---
+++ Day16-WeSplit/Day16-WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day16-WeSplit
+//
+//  Created by Nick Krasnitskiy on 21.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day16-WeSplit/Day16-WeSplit/Day16_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day16_WeSplitApp.swift
+//  Day16-WeSplit
+//
+//  Created by Nick Krasnitskiy on 21.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day16_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
