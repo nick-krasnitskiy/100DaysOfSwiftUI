c197a3bbb0b94169ccbeb4cd3da7bc34910ca8f9
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-27 08:30:57 +0300

    Initial commit.

---
+++ Day19-UnitConversions/Day19-UnitConversions/Day19_UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day19_UnitConversionsApp.swift
+//  Day19-UnitConversions
+//
+//  Created by Nick Krasnitskiy on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day19_UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
