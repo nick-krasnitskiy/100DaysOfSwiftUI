d7cbfc96a9a8521041bcdc767a21458879b159c6
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-28 08:22:37 +0300

    Initial commit.

---
+++ Day20-GuessTheFlag/Day20-GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day20-GuessTheFlag
+//
+//  Created by Nick Krasnitskiy on 28.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day20-GuessTheFlag/Day20-GuessTheFlag/Day20_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day20_GuessTheFlagApp.swift
+//  Day20-GuessTheFlag
+//
+//  Created by Nick Krasnitskiy on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day20_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
