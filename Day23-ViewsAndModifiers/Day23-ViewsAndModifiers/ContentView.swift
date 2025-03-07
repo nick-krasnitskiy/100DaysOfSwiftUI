85d0b662f20161d607e8cc484b3191470aeb229c
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-07 08:21:55 +0300

    Initial commit.

---
+++ Day23-ViewsAndModifiers/Day23-ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day23-ViewsAndModifiers
+//
+//  Created by Nick Krasnitskiy on 07.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day23-ViewsAndModifiers/Day23-ViewsAndModifiers/Day23_ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day23_ViewsAndModifiersApp.swift
+//  Day23-ViewsAndModifiers
+//
+//  Created by Nick Krasnitskiy on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day23_ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
