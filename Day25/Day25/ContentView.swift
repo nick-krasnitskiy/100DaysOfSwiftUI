20c52493e8943e8adbfe39a6123a0f7e4e9889c5
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-11 15:20:00 +0300

    Add  alternatives between prompting the player to win or lose and next button.

--- Day25/Day25/ContentView.swift
+++ Day25/Day25/ContentView.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     let variants = ["mountain.2", "newspaper", "scissors"]
-    let goals = ["Win", "Lose"]
     
     @State private var randomVariant = Int.random(in: 0...2)
+    @State private var userGoal = true
 
     var body: some View {
         VStack {
             Image(systemName: variants[randomVariant])
                 .font(.largeTitle)
+            Text(userGoal ? "Win" : "Lose")
+            Button("Next") {
+                userGoal.toggle()
+                randomVariant = Int.random(in: 0...2)
+            }
         }
     }
 }
