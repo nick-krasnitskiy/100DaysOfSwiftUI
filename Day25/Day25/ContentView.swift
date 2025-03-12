716c9a1926574ef03379843cc1f6141678977c32
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-12 14:36:21 +0300

    Add game buttons.

--- Day25/Day25/ContentView.swift
+++ Day25/Day25/ContentView.swift
@@ -14,10 +14,22 @@ struct ContentView: View {
     @State private var userGoal = true
 
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             Image(systemName: variants[randomVariant])
                 .font(.largeTitle)
             Text(userGoal ? "Win" : "Lose")
+            
+            HStack(spacing: 30) {
+                ForEach(variants, id: \.self) { name in
+                    Button {
+                        // do
+                    } label: {
+                        Image(systemName: name)
+                            .font(.largeTitle)
+                    }
+                }
+            }
+            
             Button("Next") {
                 userGoal.toggle()
                 randomVariant = Int.random(in: 0...2)
