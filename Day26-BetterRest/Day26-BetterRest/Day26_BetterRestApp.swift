ed6057abf283676e7f47dc385ce0c6c2362bca5c
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-15 21:52:52 +0300

    Initial commit.

---
+++ Day26-BetterRest/Day26-BetterRest/Day26_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day26_BetterRestApp.swift
+//  Day26-BetterRest
+//
+//  Created by Nick Krasnitskiy on 15.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day26_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
