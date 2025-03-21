94e4beeb0b189da2082cdb629c5bd8f2abc307f2
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-21 15:19:49 +0300

    Initial commit.

---
+++ Day29-WordScramble/Day29-WordScramble/Day29_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day29_WordScrambleApp.swift
+//  Day29-WordScramble
+//
+//  Created by Nick Krasnitskiy on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day29_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
