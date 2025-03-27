f050b6ca55990992703ff6b0d2d4edaf96f02006
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-27 07:19:45 +0300

    Initial commit.

---
+++ Day32-Animations/Day32-Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day32-Animations
+//
+//  Created by Nick Krasnitskiy on 27.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day32-Animations/Day32-Animations/Day32_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day32_AnimationsApp.swift
+//  Day32-Animations
+//
+//  Created by Nick Krasnitskiy on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day32_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
