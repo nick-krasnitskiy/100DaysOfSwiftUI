49c8bd66d66b8ef8d4d47367b2c7df3533e6c64c
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-08 07:57:20 +0300

    Add settings UI.

--- Day35-MultiplicationTables/Day35-MultiplicationTables/ContentView.swift
+++ Day35-MultiplicationTables/Day35-MultiplicationTables/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     let numberOfQuestions = [5, 10, 20]
     
+    @State private var isGameActive = false
+    
     @State private var selectedNumberOfQuestions = 5
     @State private var multiplicationTable = 2
     @State private var multiplicationNumber =  Int.random(in: 1...12)
@@ -20,37 +22,61 @@ struct ContentView: View {
     @State private var isGameOver = false
     
     var body: some View {
-        VStack(spacing: 30) {
-            Text("Which multiplication table do you want to practice?")
-            Stepper("\(multiplicationTable) multiplication table ", value: $multiplicationTable, in: 2...12)
-                .padding()
-            
-            Text("How many questions do you want to be asked?")
-            
-            Picker("Questions", selection: $selectedNumberOfQuestions)  {
-                ForEach(numberOfQuestions, id: \.self) {
-                    Text("\($0)")
+        NavigationStack {
+            ZStack {
+                Color.yellow
+                VStack {
+                    if !isGameActive {
+                        VStack {
+                            VStack(alignment: .leading, spacing: 20) {
+                                Text("Which multiplication table do you want to practice?")
+                                    .font(.headline)
+                                Stepper("\(multiplicationTable) tables", value: $multiplicationTable, in: 2...12)
+                                Text("How many questions do you want to be asked?")
+                                    .font(.headline)
+                                Picker("Questions", selection: $selectedNumberOfQuestions)  {
+                                    ForEach(numberOfQuestions, id: \.self) {
+                                        Text("\($0)")
+                                    }
+                                }
+                                .pickerStyle(.segmented)
+                            }
+                            Button("Start") { isGameActive = true }
+                                .frame(width: 50, height: 10)
+                                .padding()
+                                .background(.black)
+                                .foregroundStyle(.white)
+                                .clipShape(.rect(cornerRadius: 10))
+                        }
+                        .fixedSize(horizontal: false, vertical: true)
+                        .frame(width: 300, height: 250)
+                        .padding()
+                        .background(.regularMaterial)
+                        .clipShape(.rect(cornerRadius: 20))
+                        
+                    } else {
+                        VStack {
+                            Text("What is \(multiplicationTable) x \(multiplicationNumber)?")
+                            
+                            TextField("Your answer", value: $answer, format: .number)
+                            
+                            Button("Next Question") {
+                                checkAnswer()
+                                generateQuestion()
+                                answer = nil
+                            }
+                            Text("Your score: \(score)")
+                        }
+                    }
                 }
             }
-            .pickerStyle(.segmented)
-            
-            Text("What is \(multiplicationTable) x \(multiplicationNumber)?")
-            
-            TextField("Your answer", value: $answer, format: .number)
-                .padding(.horizontal, 50)
-            
-            Button("Next Question") {
-                checkAnswer()
-                generateQuestion()
-                answer = nil
+            .ignoresSafeArea()
+            .navigationTitle("Multiplication Tables")
+            .alert("Game Over", isPresented: $isGameOver) {
+                Button("Play again", action: generateQuestion)
+            } message: {
+                Text("Your final score: \(score)")
             }
-            
-            Text("Your score: \(score)")
-        }
-        .alert("Game Over", isPresented: $isGameOver) {
-            Button("Play again", action: generateQuestion)
-        } message: {
-            Text("Your final score: \(score)")
         }
     }
     
@@ -65,7 +91,6 @@ struct ContentView: View {
             }
             questionCount += 1
         }
-        
         isGameOver = true
     }
 }
