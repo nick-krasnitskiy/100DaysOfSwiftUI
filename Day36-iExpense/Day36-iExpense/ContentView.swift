7bef0e98af345172dc6ded995528b887c95279d5
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-10 08:11:29 +0300

    Initial commit.

---
+++ Day36-iExpense/Day36-iExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day36-iExpense
+//
+//  Created by Nick Krasnitskiy on 10.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day36-iExpense/Day36-iExpense/Day36_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day36_iExpenseApp.swift
+//  Day36-iExpense
+//
+//  Created by Nick Krasnitskiy on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day36_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
