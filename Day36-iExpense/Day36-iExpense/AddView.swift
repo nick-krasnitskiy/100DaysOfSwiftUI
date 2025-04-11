85048bd5e9d7540a4e0cd8a367d33fbc4f16a183
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-11 12:49:47 +0300

    Sharing an observed object with a new view.

---
+++ Day36-iExpense/Day36-iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  Day36-iExpense
+//
+//  Created by Nick Krasnitskiy on 11.04.2025.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
