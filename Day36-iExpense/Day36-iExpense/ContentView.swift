a3ef06e87aea0d7482af915d2c796e1e245ab12d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-11 11:02:26 +0300

    Build a list we can delete from.

--- Day36-iExpense/Day36-iExpense/ContentView.swift
+++ Day36-iExpense/Day36-iExpense/ContentView.swift
@@ -8,22 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var user = User(firstName: "Nick", lastName: "Krasnitskiy")
+    @State private var expenses = Expenses()
     
     var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
-            
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
             }
         }
     }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
 }
 
-struct User: Codable {
-    let firstName: String
-    let lastName: String
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
 }
 
 #Preview {
