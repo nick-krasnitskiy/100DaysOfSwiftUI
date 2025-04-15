573ff8cf12aa4271ba54fe13ed7b2bfbf6cfc519
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-15 08:04:11 +0300

    Initial commit.

---
+++ Day39-Moonshot/Day39-Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day39-Moonshot
+//
+//  Created by Nick Krasnitskiy on 15.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day39-Moonshot/Day39-Moonshot/Day39_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day39_MoonshotApp.swift
+//  Day39-Moonshot
+//
+//  Created by Nick Krasnitskiy on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day39_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
