dca0a5ef90aa79f704ac18b4ffda0d4e46b6894f
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-17 10:36:38 +0300

    Extract horizontal scroll view in MissionView into their own new SwiftUI view.

---
+++ Day39-Moonshot/Day39-Moonshot/HorizontalScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalScrollView.swift
+//  Day39-Moonshot
+//
+//  Created by Nick Krasnitskiy on 17.04.2025.
+//
+
+import SwiftUI
+
+struct HorizontalScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalScrollView()
+}
