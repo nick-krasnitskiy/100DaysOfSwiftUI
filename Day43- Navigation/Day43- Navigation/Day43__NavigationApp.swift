ac93c323409e5a5ba7ef747a0d75ff648b1e4e94
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-18 07:23:17 +0300

    Initial commit.

---
+++ Day43- Navigation/Day43- Navigation/Day43__NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day43__NavigationApp.swift
+//  Day43- Navigation
+//
+//  Created by Nick Krasnitskiy on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Day43__NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
