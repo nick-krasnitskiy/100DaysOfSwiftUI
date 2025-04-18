95e521f98a71c10ba14c85ac7959dba369a3fc3f
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-18 08:09:34 +0300

    Handling navigation the smart way with navigationDestination().

--- Day43- Navigation/Day43- Navigation/ContentView.swift
+++ Day43- Navigation/Day43- Navigation/ContentView.swift
@@ -10,25 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<1000) { i in
-                NavigationLink("Tap Me") {
-                    DetailView(number: i)
-                }
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
             }
         }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View {
-        Text("Detail View \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view \(number)")
     }
 }
 
