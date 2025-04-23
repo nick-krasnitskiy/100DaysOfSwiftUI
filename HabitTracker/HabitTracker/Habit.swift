1b1eb3e9bafa133f85bf5ba946cad33d4b3bdc86
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-23 16:43:59 +0300

    Add a Habit struct.

---
+++ HabitTracker/HabitTracker/Habit.swift
@@ -0,0 +1,13 @@
+//
+//  Habit.swift
+//  HabitTracker
+//
+//  Created by Nick Krasnitskiy on 23.04.2025.
+//
+
+import Foundation
+
+struct Habit {
+    let title: String
+    let description: String
+}
