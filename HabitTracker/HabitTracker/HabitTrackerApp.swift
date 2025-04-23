e0bbd8259e146960810e057b07f1bde1490e749b
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-23 16:41:23 +0300

    Initial commit.

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Nick Krasnitskiy on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
