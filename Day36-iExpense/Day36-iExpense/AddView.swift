749a6efd539c695b331a41b9cfe2705e8799f0bb
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-23 07:23:18 +0300

    Let users edit their issue name in the navigation title rather than a separate textfield.

--- Day36-iExpense/Day36-iExpense/AddView.swift
+++ Day36-iExpense/Day36-iExpense/AddView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct AddView: View {
-    @State private var name = ""
+    @State private var name = "New Expense"
     @State private var type = "Personal"
     @State private var amount = 0.0
     
@@ -20,8 +20,6 @@ struct AddView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
-                
                 Picker("Type", selection: $type) {
                     ForEach(types, id: \.self) {
                         Text($0)
@@ -31,7 +29,8 @@ struct AddView: View {
                 TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add new expense")
+            .navigationTitle($name)
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
