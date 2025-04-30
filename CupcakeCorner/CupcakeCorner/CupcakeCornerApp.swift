aed04ac630abc57c38eb44465fda81775c317ee8
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-04-30 16:42:06 +0300

    Initial commit.

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Nick Krasnitskiy on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
