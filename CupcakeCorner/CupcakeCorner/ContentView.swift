8a2f13578eb8b988f4976200d87989f1c9d61765
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-05-06 14:29:31 +0300

    Add Codable conformance to an @Observable class.

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,28 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
-    
+   
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account...")
-                }
-            }
-            .disabled(disableForm)
-        }
+        Button("Encode Taylor", action: encodeTaylor)
     }
+    
+    func encodeTaylor() {
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
+    }
+}
+
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    
+    var name = "Taylor"
 }
 
 #Preview {
