0eb1ae0db89f82dba31896cdff0d0c1566c6ee9e
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-05-06 15:24:57 +0300

    Add haptic effects.

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -6,28 +6,51 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
-   
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Button("Encode Taylor", action: encodeTaylor)
+        Button("Tap Me", action: complexSucess)
+            .onAppear(perform: prepareHaptics)
     }
     
-    func encodeTaylor() {
-        let data = try! JSONEncoder().encode(User())
-        let str = String(decoding: data, as: UTF8.self)
-        print(str)
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSucess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameterCurves: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.stop(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
 }
 
-@Observable
-class User: Codable {
-    enum CodingKeys: String, CodingKey {
-        case _name = "name"
-    }
-    
-    var name = "Taylor"
-}
+
+
 
 #Preview {
     ContentView()
