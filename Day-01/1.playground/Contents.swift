04d8afde6fbacee6f150958b1c887846449107d3
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-18 09:15:50 +0300

    Create variables and constants.

--- Day-01/1.playground/Contents.swift
+++ Day-01/1.playground/Contents.swift
@@ -1,3 +1,28 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+var name = "Tedd"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+// character = "Eloise"
+// character = "Francesca"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
