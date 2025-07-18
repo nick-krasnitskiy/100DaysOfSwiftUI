e474ea9ce5d30cda9037a4fbcea1e1e03ebd9c58
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-18 09:49:12 +0300

    Store whole numbers.

--- Day-01/1.playground/Contents.swift
+++ Day-01/1.playground/Contents.swift
@@ -60,3 +60,35 @@ The best laid schemes
 Gang aft agley
 """
 
+// Store whole numbers
+
+let score = 10
+let reallyBig = 1000000000
+let reallyBig2 = 1_00_000_00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+
+counter -= 10
+print(counter)
+
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
+
+
