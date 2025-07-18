08678b67b6c41a95d636eca46a94902e3f914c6b
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-18 10:13:05 +0300

    Store decimal numbers.

--- Day-01/1.playground/Contents.swift
+++ Day-01/1.playground/Contents.swift
@@ -91,4 +91,30 @@ let number = 120
 print(number.isMultiple(of: 3))
 print(120.isMultiple(of: 3))
 
+// Store decimal numbers
 
+let number2 = 0.1 + 0.2
+print(number2)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+let d = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name2 = "Nicolas Cage"
+name2 = "John Travolta"
+// name2 = 57
+
+var rating = 5.0
+rating *= 2
+
+var myInt = 1
+var myDouble = 1.0
+
+var meaningOfLife2 = 42
+// meaningOfLife2 = "Forty two"
