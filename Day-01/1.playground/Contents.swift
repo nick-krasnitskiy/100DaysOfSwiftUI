903e7961bb05ffe98161b3df5ac6a3247dcaadb8
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-18 09:38:56 +0300

    Create strings.

--- Day-01/1.playground/Contents.swift
+++ Day-01/1.playground/Contents.swift
@@ -2,6 +2,8 @@ import Cocoa
 
 var greeting = "Hello, playground"
 
+// Create variables and constants
+
 var name = "Tedd"
 name = "Rebecca"
 name = "Keeley"
@@ -26,3 +28,35 @@ let meaningOfLife = "How many roads must a man walk down?"
 var favoriteShow = "Orange is the New Black"
 favoriteShow = "The Good Place"
 favoriteShow = "Doctor Who"
+
+// Create strings
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+tht life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLenght = actor.count
+print(nameLenght)
+
+print(result.uppercased())
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+var quote2 = "Change the world by being yourself"
+
+var burns = """
+The best laid schemes
+0' mice and men
+Gang aft agley
+"""
+
