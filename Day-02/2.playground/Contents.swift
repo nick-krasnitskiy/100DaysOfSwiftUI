34ca705abb3be0c054ca7f9e3be0d841a6ff2e6d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-19 18:35:32 +0300

    Store truth with Booleans.

--- Day-02/2.playground/Contents.swift
+++ Day-02/2.playground/Contents.swift
@@ -1,3 +1,31 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Store truth with Booleans
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let goodDogs = true
+let gameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOver2 = false
+print(gameOver2)
+
+gameOver2.toggle()
+print(gameOver2)
+
+
