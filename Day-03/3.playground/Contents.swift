3e869c1c9b1250a2926b51c13447c6956826f3b0
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-20 20:17:47 +0300

    Store ordered data in arrays.

--- Day-03/3.playground/Contents.swift
+++ Day-03/3.playground/Contents.swift
@@ -1,3 +1,68 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Store ordered data in arrays
+
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Adrian")
+
+beatles.append("Allen")
+beatles.append("Adrian")
+beatles.append("Novall")
+beatles.append("Vivian")
+
+// temperatures.append("Chris")
+
+let firstBeatle = beatles[0]
+let firstNumber = numbers[0]
+// let noAllowed = firstBeatle + firstNumber
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+print(scores[1])
+
+var albums = Array<String>()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+var albums2 = [String]()
+albums2.append("Folklore")
+albums2.append("Fearless")
+albums2.append("Red")
+
+var albums3 = ["Folklore"]
+albums3.append("Fearless")
+albums3.append("Red")
+
+print(albums.count)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+
+characters.remove(at: 2)
+print(characters.count)
+
+characters.removeAll()
+print(characters.count)
+
+let bondMovies = ["Casino Royale", "Spectre", "No Time To Die"]
+print(bondMovies.contains("Frozen"))
+
+let cities = ["London", "Tokyo", "Rome", "Budapest"]
+print(cities.sorted())
+
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let reversedPresidents = presidents.reversed()
+print(reversedPresidents)
+print(Array(reversedPresidents))
