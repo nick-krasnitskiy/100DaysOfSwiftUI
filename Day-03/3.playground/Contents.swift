370f0028d5b5b40219582d29dea106174cf32d04
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-21 07:30:57 +0300

    Create and use enums.

--- Day-03/3.playground/Contents.swift
+++ Day-03/3.playground/Contents.swift
@@ -129,4 +129,31 @@ people2.insert("Tom Cruise")
 people2.insert("Nicolas Cage")
 people2.insert("Samuel L Jackson")
 
+// Create and use enums
+
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+var day = Weekday.monday
+day = Weekday.tuesday
+day = Weekday.friday
+
+enum Weekday2 {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day2 = Weekday2.monday
+day2 = .tuesday
+day2 = .friday
+
 
