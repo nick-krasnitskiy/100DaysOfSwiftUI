173bd14957f76115d2ed786ee2a654930381c450
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-21 07:19:46 +0300

    Use sets for fast data lookup.

--- Day-03/3.playground/Contents.swift
+++ Day-03/3.playground/Contents.swift
@@ -117,3 +117,16 @@ let results = [
 ]
 
 let historyResult = results["history", default: 0]
+
+// Use sets for fast data lookup
+
+let people = Set(["Denzel Wanshington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+print(people)
+
+var people2 = Set<String>()
+people2.insert("Denzel Washington")
+people2.insert("Tom Cruise")
+people2.insert("Nicolas Cage")
+people2.insert("Samuel L Jackson")
+
+
