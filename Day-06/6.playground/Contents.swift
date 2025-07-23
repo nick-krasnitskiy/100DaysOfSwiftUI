f3e62e5e12dcc0fb8d8dea3d21f21c3e4f0373e5
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-23 10:13:11 +0300

    Checkpoint 3.

--- Day-06/6.playground/Contents.swift
+++ Day-06/6.playground/Contents.swift
@@ -152,3 +152,17 @@ outerLoop: for option1 in options {
         }
     }
 }
+
+// Checkpoint 3
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
