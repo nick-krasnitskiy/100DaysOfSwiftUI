a1393ffbbf1c7b0b3a1537d44491c5eb11290b9d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-23 09:36:48 +0300

    Skip loop items with break and continue.

--- Day-06/6.playground/Contents.swift
+++ Day-06/6.playground/Contents.swift
@@ -93,3 +93,62 @@ while roll != 20 {
 
 // if we're here it means the loop ended - we got a 20!
 print("Critical hit!")
+
+// Skip loop items with break and continue
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+    
+    print("Found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+        
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+let scores = [1, 8, 4, 3, 0, 5, 2]
+var count = 0
+
+for score in scores {
+    if score == 0 {
+        break
+    }
+    
+    count += 1
+}
+
+print("You had \(count) scores before you got 0")
+
+let options = ["up", "down", "left", "right"]
+
+let secretCombination = ["up", "up", "right"]
+
+outerLoop: for option1 in options {
+    for option2 in options {
+        for option3 in options {
+            print("In loop")
+            let attempt = [option1, option2, option3]
+            
+            if attempt == secretCombination {
+                print("The combination is \(attempt)!")
+                break outerLoop
+            }
+        }
+    }
+}
