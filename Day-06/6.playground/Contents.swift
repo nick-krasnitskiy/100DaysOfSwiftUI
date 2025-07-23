ba09947cdd4806e21c2188ea2730ae8954504815
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-23 08:19:23 +0300

    Use a for loop to repeat work.

--- Day-06/6.playground/Contents.swift
+++ Day-06/6.playground/Contents.swift
@@ -1,3 +1,68 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Use a for loop to repeat work
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os).")
+}
+
+for name in platforms {
+    print("Swift works great on \(name).")
+}
+
+for platform in platforms {
+    print("Swift works great on \(platform).")
+}
+
+for i in 1...12 {
+    print("5 x \(i) is \(5 * i)")
+}
+
+for i in 1...12 {
+    print("The \(i) times table:")
+    
+    for j in 1...12 {
+        print(" \(j) x \(i) is \(j * i)")
+    }
+    
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
+
+let names = ["Sterling", "Cyril", "Lana", "Ray", "Pam"]
+
+for name in names {
+    print("\(name) is a secret agent")
+}
+
+for _ in names {
+    print("[CENSORED] is a secret agent!")
+}
+
+let names2 = ["Piper", "Alex", "Suzanne", "Gloria"]
+
+print(names2[0])
+print(names2[1...3])
+
+print(names2[1...])
