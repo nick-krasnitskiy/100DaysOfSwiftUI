bd53bea90fcedd066548f814bf3b715815ec10e8
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-25 08:28:53 +0300

    Return values from functions.

--- Day-07/7.playground/Contents.swift
+++ Day-07/7.playground/Contents.swift
@@ -44,4 +44,56 @@ func printTimesTables2(number: Int, end: Int) {
 
 printTimesTables2(number: 5, end: 20)
 
+// Return values from functions
+
+let root = sqrt(169)
+print(root)
+
+func rollDice() -> Int {
+    Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+func areLettersIdentical(string1: String, string2: String) -> Bool {
+    let first = string1.sorted()
+    let second = string2.sorted()
+    return first == second
+}
+
+func areLettersIdentical2(string1: String, string2: String) -> Bool {
+    return string1.sorted() == string2.sorted()
+}
+
+func areLettersIdentical3(string1: String, string2: String) -> Bool {
+    string1.sorted() == string2.sorted()
+}
+
+func pythagoras(a: Double, b: Double) -> Double {
+    let input = a * a + b * b
+    let root = sqrt(input)
+    return root
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+func pythagoras2(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+func doMath() -> Int {
+    return 5 + 5
+}
+
+func doMoreMath() -> Int {
+    5 + 5
+}
+
+func greet(name: String) -> String {
+    let response = name == "Taylor Swift" ? "Oh wow!" : "Hello, \(name)"
+    return response
+}
+
 
