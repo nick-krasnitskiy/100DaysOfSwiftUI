dfe9b58a297fa4c541a0697132e5fd202f62c1d5
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-25 07:53:42 +0300

    Reuse code with functions.

--- Day-07/7.playground/Contents.swift
+++ Day-07/7.playground/Contents.swift
@@ -1,3 +1,47 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Reuse code with functions
+
+print("Welcome to my app!")
+print("By default This prints out a conversation")
+print("chart from centimeters to inches, but you")
+print("can also set a custom range if you want.")
+
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default This prints out a conversation")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
+}
+
+showWelcome()
+
+let number = 139
+
+if number.isMultiple(of: 2) {
+    print("Even")
+} else {
+    print("Odd")
+}
+
+let roll = Int.random(in: 1...20)
+
+func printTimesTables(number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5)
+
+func printTimesTables2(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables2(number: 5, end: 20)
+
+
