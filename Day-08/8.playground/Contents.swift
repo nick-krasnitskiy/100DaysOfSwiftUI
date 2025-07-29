aa17b260d60766052ca9f507ec8adc0ede83866b
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-29 09:34:35 +0300

    Handle errors in functions.

--- Day-08/8.playground/Contents.swift
+++ Day-08/8.playground/Contents.swift
@@ -26,3 +26,48 @@ func findDirections(from: String, to: String, route: String = "fastest", avoidHi
 findDirections(from: "London", to: "Glasgow")
 findDirections(from: "London", to: "Glasgow", route: "scenic")
 findDirections(from: "London", to: "Glasgow", route: "scenic", avoidHighways: true)
+
+
+// Handle errors in functions
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let string = "12345"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch {
+    print("There was an error.")
+}
+
+do {
+    let result2 = try checkPassword(string)
+    print("Password rating: \(result2)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I tave the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
