f490a1bcdfed97bc795f5497a5c6119fa79aba5c
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-07-30 14:36:05 +0300

    Create and use closures.

--- Day-09/9.playground/Contents.swift
+++ Day-09/9.playground/Contents.swift
@@ -3,3 +3,80 @@ import Cocoa
 var greeting = "Hello, playground"
 
 // Create and use closures.
+
+func greetUser() {
+    print("Hi there!")
+}
+
+greetUser()
+
+var greetCopy = greetUser
+greetCopy()
+
+let sayHello = {
+    print("Hi there!")
+}
+
+sayHello()
+
+let sayHello2 = { (name: String) -> String in
+    "Hi \(name)"
+}
+
+
+var greetCopy2: () -> Void = greetUser
+
+func getUserData(for id: Int) -> String {
+    if id == 1989 {
+        return "Taylor Swift"
+    } else {
+        return "Anonymous"
+    }
+}
+ 
+let data: (Int) -> String = getUserData
+let user = data(1989)
+print(user)
+
+sayHello2("Taylor")
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+func captainFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+}
+
+let captainFirstTeam = team.sorted(by: captainFirstSorted)
+print(captainFirstTeam)
+
+let captainFirstTeam2 = team.sorted(by: {(name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+})
+
+let payment = { (user: String) in
+    print("Paying \(user)...")
+}
+
+let payment2 = { (user: String) -> Bool in
+    print("Paying \(user)...")
+    return true
+}
+
+let payment3 = { () -> Bool in
+    print("Paying an anonymous person...")
+    return true
+}
