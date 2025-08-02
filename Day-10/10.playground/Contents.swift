7a68a0a27d940c919644553d6d76545fcbcd7910
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-08-02 22:12:24 +0300

    Create your own structs.

--- Day-10/10.playground/Contents.swift
+++ Day-10/10.playground/Contents.swift
@@ -1,3 +1,49 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Create your own structs
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title) (\(year) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+struct Employee {
+    let name: String
+    var vacationRemaining = 14
+    
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation!")
+            print("days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enouht days remainingю")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
+
+var archer1 = Employee(name: "Sterling Archer", vacationRemaining: 14)
+var archer2 = Employee.init(name: "Sterling Archer", vacationRemaining: 14)
+
+let kane = Employee(name: "Lana Kane")
+let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
