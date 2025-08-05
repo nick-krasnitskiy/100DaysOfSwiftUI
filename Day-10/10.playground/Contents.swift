deebc1de9a964fca4f9f3d23f3bf878ef49a2971
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-08-05 15:14:39 +0300

    Take action when a property changes.

--- Day-10/10.playground/Contents.swift
+++ Day-10/10.playground/Contents.swift
@@ -87,3 +87,50 @@ print(archer4.vacationRemaing)
 
 archer4.vacationRemaing = 5
 print(archer4.vacationAllocated)
+
+// Take action when a property changes
+
+struct Game {
+    var score = 0
+}
+
+var game = Game()
+
+game.score += 10
+print("Score is now \(game.score)")
+
+game.score -= 3
+print("Score is now \(game.score)")
+game.score += 1
+
+struct Game2 {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game2 = Game2()
+game2.score += 10
+game2.score -= 3
+game2.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Tsh S")
