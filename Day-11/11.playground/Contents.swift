db015e58d05ab274a0e2a5552b8d9212baeaac47
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-08-06 10:14:00 +0300

    Limit access to internal data using access control.

--- Day-11/11.playground/Contents.swift
+++ Day-11/11.playground/Contents.swift
@@ -1,3 +1,35 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Limit access to internal data using access control
+
+struct BankAccount {
+    private(set) var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+
+let sucess = account.withdraw(amount: 200)
+
+if sucess {
+    print("Withdrew money sucessfully")
+} else {
+    print("Failed to get the money")
+}
+
+// account.funds -= 100
